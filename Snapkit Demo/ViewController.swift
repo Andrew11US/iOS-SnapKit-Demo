38f15d8b017801754dbef6fc2f05eb1928b008d9
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-08-09 21:54:34 +0200

    Initial Commit

---
+++ Snapkit Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snapkit Demo
+//
+//  Created by Andrew on 8/9/20.
+//  Copyright © 2020 Andrii Halabuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
